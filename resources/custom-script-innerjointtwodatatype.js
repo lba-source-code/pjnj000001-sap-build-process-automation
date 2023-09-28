8b995f7c554e3cb9a9f254e4cec2548dbaffcd93
Luis Benavides Andrade <luis.benavides.andrade@gmail.com>
2023-09-28 14:15:34 +0200

    dev-luisbenavidesandrade: updata join script

--- resources/custom-script-innerjointtwodatatype.js
+++ resources/custom-script-innerjointtwodatatype.js
@@ -1,2 +1,17 @@
 //dev-luisbenavidesandrade
 
+const salesOrdersHeader = {
+    0: { orderNumber: 'SO0001', orderCustomer: 'Customer1' },
+    1: { orderNumber: 'SO0002', orderCustomer: 'Customer2' },
+};
+const salesOrdersItems = {
+    0: { orderNumber: 'SO0001', orderPosition: 10 },
+    1: { orderNumber: 'SO0001', orderPosition: 20 },
+    2: { orderNumber: 'SO0002', orderPosition: 10 },
+    3: { orderNumber: 'SO0002', orderPosition: 20 },
+};
+
+const outputObjct = Object.assign(salesOrdersItems, salesOrdersHeader);
+
+console.log(outputObjct);
+
